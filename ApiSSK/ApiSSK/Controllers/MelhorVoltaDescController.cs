763c7b577300aa74c0a8dcafe010a0f0e7a79b4b
FELIPE ALMEIDA <FALUCAS@trf3.jus.br>
2024-09-16 15:38:43 -0300

    new item

---
+++ ApiSSK/ApiSSK/Controllers/MelhorVoltaDescController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiSSK.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MelhorVoltaDescController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<MelhorVoltaDescModel>>> GetAllMelhoresVoltasDesc()
+        {
+            var mv = new List<MelhorVoltaDescModel>()
+            {
+                new MelhorVoltaDescModel()
+                {
+                    MvdId = 1,
+                    MvdDesc = "Estreia",
+                },
+                new MelhorVoltaDescModel()
+                {
+                    MvdId = 2,
+                    MvdDesc = "Recorde",
+                },
+                new MelhorVoltaDescModel()
+                {
+                    MvdId = 3,
+                    MvdDesc = "MV Seco",
+                },
+                new MelhorVoltaDescModel()
+                {
+                    MvdId = 4,
+                    MvdDesc = "MV Misto",
+                },
+                new MelhorVoltaDescModel()
+                {
+                    MvdId = 5,
+                    MvdDesc = "MV Chuva",
+                }
+            };
+
+            return Ok(mv);
+        }
+    }
+}
---
+++ ApiSSK/ApiSSK/Models/MelhorVoltaDescModel.cs
@@ -0,0 +1,7 @@
+﻿using System.Globalization;
+
+public partial class MelhorVoltaDescModel
+{
+    public int MvdId { get; set; }
+    public string MvdDesc { get; set; }
+}
