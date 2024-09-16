1a5e2c715edf87d9b45a969d8a0b86f16814e8cd
FELIPE ALMEIDA <FALUCAS@trf3.jus.br>
2024-09-16 15:21:01 -0300

    new itens

---
+++ ApiSSK/ApiSSK/Repositories/ClimaRepository.cs
@@ -0,0 +1,20 @@
+﻿using ApiSSK.Data;
+using ApiSSK.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiSSK.Repositories
+{
+    public class ClimaRepository : IClimaRepository
+    {
+        private readonly DataContext _dbContext;
+        public ClimaRepository(DataContext dataContext)
+        {
+            _dbContext = dataContext;
+        }
+
+        public async Task<List<ClimaModel>> GetAllClimas()
+        {
+            return await _dbContext.Climas.ToListAsync();
+        }
+    }
+}
---
+++ ApiSSK/ApiSSK/Repositories/Interfaces/IClimaRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiSSK.Repositories.Interfaces
+{
+    public interface IClimaRepository
+    {
+        Task<List<ClimaModel>> GetAllClimas();
+
+        //buscar por id
+        //adicionar
+        //modificar
+        //deletar
+    }
+}
