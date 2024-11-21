0b2f66f42b52e68c8f09789483726e7dfa310afc
Felipe Almeida Lucas <felipea.lucas@outlook.com>
2024-11-20 23:32:11 -0300

    new migrations

---
+++ ApiSSK/ApiSSK/Migrations/20241121023051_RemoveUniqueIndexFromCategoria.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiSSK.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUniqueIndexFromCategoria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_CATEGORIAS_STA_ID",
+                table: "CATEGORIAS");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_CATEGORIAS_STA_ID",
+                table: "CATEGORIAS",
+                column: "STA_ID",
+                unique: true);
+        }
+    }
+}
